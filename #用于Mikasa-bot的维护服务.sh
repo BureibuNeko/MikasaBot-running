3922498a7ab26bc307ac4bfe52efc275a03afc9b
Alexander Vasiliev <79738554+BureibuNeko@users.noreply.github.com>
2021-10-06 00:20:46 +0800

    Add files via upload

---
+++ #用于Mikasa-bot的维护服务.sh
@@ -0,0 +1,12 @@
+#用于Mikasa-bot的维护服务
+become zh-fwdreview-bot 
+cd LilyWhiteBot
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+nvm use 14.16.0
+
+npm install 
+node main.js
